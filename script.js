9bfc289fa6277675aebefc200dd061b1bc26ef4c
Shivam <shivamprajapati2006@gmail.com>
2026-07-27 23:17:54 +0530

    Contact Form Added

--- script.js
+++ script.js
@@ -4,3 +4,43 @@ const navList = document.getElementById("navList");
 hamburger.addEventListener("click", () => {
     navList.classList.toggle("active");
 });
+
+const form = document.getElementById("contactForm");
+
+form.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const params = {
+
+        name: document.getElementById("name").value,
+
+        email: document.getElementById("email").value,
+
+        subject: document.getElementById("subject").value,
+
+        message: document.getElementById("message").value
+
+    };
+
+    emailjs.send(
+        "service_7cje1g3",
+        "template_npipsci",
+        params
+    )
+    .then(function(){
+
+        alert("Message sent successfully!");
+
+        form.reset();
+
+    })
+    .catch(function(error){
+
+        alert("Failed to send message.");
+
+        console.log(error);
+
+    });
+
+});
